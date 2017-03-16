30637da0bd9780276f81c21f87755a07aebf1b6a
宋伟 <610347922@qq.com>
2017-03-16 16:58:57 +0800

    1. 管理端的路由

--- routes/manage-router.js
+++ routes/manage-router.js
@@ -7,7 +7,17 @@
 let router = require('express').Router(),
   manageRouter = require('./manageRoutes/manageRoute');
 
-router.get('/test', manageRouter.managerIndexPage);
+// 管理端的页面
+router.get('/index', manageRouter.manageIndexPage);
+router.get('/contact', manageRouter.manageContactPage);
+router.get('/news', manageRouter.manageNewPage);
+router.get('/example', manageRouter.manageExamplePage);
+router.get('/configure', manageRouter.manageConfigurePage);
+router.get('/certificate', manageRouter.manageCertificatePage);
+router.get('/download', manageRouter.manageDownloadPage);
+router.get('/product', manageRouter.manageProductPage);
+router.get('/aboutme', manageRouter.manageAboutMePage);
+router.get('/technology', manageRouter.manageTechnologyPage);
 
 
 module.exports = router;
--- routes/manageRoutes/manageRoute.js
+++ routes/manageRoutes/manageRoute.js
@@ -5,18 +5,123 @@
 let pub = {};
 
 /**
- * 管理端的主页面
+ * 管理端的页面
  * @param req
  * @param res
  * @param next
  */
-pub.managerIndexPage = (req, res, next) => {
-  res.render('serverPage/test',{
+pub.manageIndexPage = (req, res, next) => {
+  res.render('serverPage/test', {
+    "layout": false
+  });
+};
+
+/**
+ * 联系方式的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageContactPage = (req, res, next) => {
+  res.render('', {
     "layout": false
   })
-  // res.render('serverPage/layout', {
-  //   layout: false
-  // })
+};
+
+/**
+ * 新闻相关的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageNewPage = (req, res, next) => {
+  res.render('', {
+    "layout": false
+  })
+};
+
+/**
+ * 成功案例的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageExamplePage = (req, res, next) => {
+  res.render('', {
+    "layout": false
+  })
+};
+
+/**
+ * 系统配置的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageConfigurePage = (req, res, next) => {
+  res.render('', {
+    "layout": false
+  })
+};
+
+/**
+ * 资质证书的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageCertificatePage = (req, res, next) => {
+  res.render('', {
+    "layout": false
+  })
+};
+
+/**
+ * 下载文件的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageDownloadPage = (req, res, next) => {
+  res.render('', {
+    "layout": false
+  })
+};
+
+/**
+ * 产品相关的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageProductPage = (req, res, next) => {
+  res.render('', {
+    "layout": false
+  })
+};
+
+/**
+ * 关于我们的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageAboutMePage = (req, res, next) => {
+  res.render('', {
+    "layout": false
+  })
+};
+
+/**
+ * 技术支持的页面
+ * @param req
+ * @param res
+ * @param next
+ */
+pub.manageTechnologyPage = (req, res, next) => {
+  res.render('', {
+    "layout": false
+  })
 };
 
 module.exports = pub;
